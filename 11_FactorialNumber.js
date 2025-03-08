1a40d5508538a99e6b324f8dbfa92f2372b3d0e3
montumeena141 <montumeena141@gmail.com>
2025-03-08 16:51:29 +0530

    first commit

---
+++ 11_FactorialNumber.js
@@ -0,0 +1,8 @@
+// Factorial of a Number using a loop
+var factorial = 1;
+var numFactorial = 5;
+for (let i = 1; i <= numFactorial; i++) {
+    factorial *= i;
+}
+console.log(`Factorial of ${numFactorial} is:`, factorial);
+
