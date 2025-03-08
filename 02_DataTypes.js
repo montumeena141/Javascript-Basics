fd68d3e5b4c3008892609ddc8c5eea1549d2892f
montumeena141 <montumeena141@gmail.com>
2025-03-08 16:44:52 +0530

    first commit

---
+++ 02_DataTypes.js
@@ -0,0 +1,22 @@
+//data types that are present in js
+//primitive and non primitive
+
+//primitive data types- number.string,boolean,null,underdefined
+//non-primitive- objects and arrays
+
+var number=2;
+var string="montumeena";
+var boolean=true;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+
+var arr=[1,2,3,4,5];
+
+
+var objects={
+    //key:value
+    name:"montu",
+    age:19,
+}
