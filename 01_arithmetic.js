30c2b73e6e24ee1bebc9c2d98fc011653206f383
montumeena141 <montumeena141@gmail.com>
2025-03-08 16:41:50 +0530

    first commit

---
+++ 01_arithmetic.js
@@ -0,0 +1,23 @@
+let a=12;
+let b=3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(a--);
+console.log(a%b);
+
+var num1;
+var num2=2;
+num1=num2;  //assignment
+console.log(num1);
+console.log(num2); 
+// console.log(c);  //define first# Javascript
+
+let x=10;
+console.log(x+=10); //x=x+10
