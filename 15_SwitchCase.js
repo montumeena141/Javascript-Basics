1a30f5a6a1fef2bd5dbdfe090dfa6cd2fd9a251d
montumeena141 <montumeena141@gmail.com>
2025-03-08 16:53:31 +0530

    first commit

---
+++ 15_SwitchCase.js
@@ -0,0 +1,18 @@
+// Switch case example: Similar to if-else, compares expression values
+
+var value = 2; // Define the value to be checked
+
+switch (value) {
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    case 3:
+        console.log(3);
+        break;
+    default:
+        console.log("Default case");
+        break;
+}
